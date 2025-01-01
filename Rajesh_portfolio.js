c811e8f1749fbe35bc144de26dd766ff354a3f5c
Rajesh Verma <163795578+Raje5hverma@users.noreply.github.com>
2025-01-02 00:08:17 +0530

    Add files via upload

---
+++ Rajesh_portfolio.js
@@ -0,0 +1,20 @@
+var tablinks = document.getElementsByClassName("title_para");
+var tabcontents = document.getElementsByClassName("tab_contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu=document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right="0";
+}
+function closemenu(){
+    sidemenu.style.right="-200px";
+}
